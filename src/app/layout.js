3f70d7bdec41721ddcc5268bc4e681489ab31402
Frank Corona Prendes <frank.corona@pm.me>
2023-03-23 20:16:52 -0300

    chore: install and configure tailwind css
    Signed-off-by: Frank Corona Prendes <frank.corona@pm.me>

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,9 @@ import "./globals.css";
 export const metadata = {
   title: "Google",
   description: "Google clone built with Next.js",
+  icons: {
+    icon: "/favicon.svg",
+  },
 };
 
 export default function RootLayout({ children }) {
