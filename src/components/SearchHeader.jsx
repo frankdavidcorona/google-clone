2cc9cba26e47fee5ac15c7a9a82475c9e77c181a
Frank Corona Prendes <frank.corona@pm.me>
2023-03-24 15:31:15 -0300

    feat: update the search header component

--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -1,3 +1,35 @@
+import { Cog8ToothIcon, Squares2X2Icon } from "@heroicons/react/24/solid";
+import Image from "next/image";
+import Link from "next/link";
+import SearchBox from "./SearchBox";
+import SearchHeaderOptions from "./SearchHeaderOptions";
+
 export default function SearchHeader() {
-  return <div>SearchHeader</div>;
+  return (
+    <header className="sticky top-0 bg-white">
+      <div className="flex w-full p-6 items-center justify-between">
+        <Link href={"/"}>
+          <Image
+            priority
+            src={"/google_banner.png"}
+            width={120}
+            height={40}
+            alt="google banner"
+            className="w-auto h-auto"
+          />
+        </Link>
+        <div className="flex-1">
+          <SearchBox />
+        </div>
+        <div className="hidden sm:inline-flex">
+          <Cog8ToothIcon className="w-10 h-10 header-icon" />
+          <Squares2X2Icon className="w-10 h-10 header-icon" />
+        </div>
+        <button className="ml-2 bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md transition-all duration-200 ">
+          Sign in
+        </button>
+      </div>
+      <SearchHeaderOptions />
+    </header>
+  );
 }
